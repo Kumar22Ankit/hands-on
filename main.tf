9c834608ed610f7c639834455c9d9a3d9d9c9b70
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-23 12:55:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "ec2" {
   source         = "./modules/ec2"
   ami_id         = "ami-0e2c8caa4b6378d8c"
   instance_type  = "t2.micro"
-  instance_name  = "terrateam-ec2-example"
+  instance_name  = "terrateam-ec2-handson"
 }
